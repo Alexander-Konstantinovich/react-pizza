81f17e99e0911c43070645ce493f43a4f8c2d597
Alexander-Konstantinovich <shushpanov.ak@yandex.ru>
2024-05-18 17:50:44 +0300

    feat: add state for input search

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import Home from './pages/Home.jsx';
 import Cart from './pages/Cart.jsx';
 
 function App() {
+
+	const [searchValue, setSearchValue] = React.useState('')
 	return (
 		<div className='wrapper'>
-			<Header />
+			<Header searchValue={searchValue} setSearchValue={setSearchValue} />
 			<div className='content'>
 				<Routes>
 					<Route path='/' element={<Home />} />
